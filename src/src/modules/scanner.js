9b23ee8f0912f7425dd7fbd836160316c7085897
Geoverse-92 <m.gajdzicki92@gmail.com>
2026-09-07 11:47:56 +0200

    Delete src directory
    Signed-off-by: Geoverse-92 <m.gajdzicki92@gmail.com>

--- src/src/modules/scanner.js
+++
@@ -1,9 +0,0 @@
-import { AppState } from '../core/state.js';
-
-export function processReceipt(receiptCode) {
-    const state = AppState.load();
-    state.inventory.push({ name: receiptCode, icon: "🧾" });
-    state.player.gold += 200;
-    AppState.save(state);
-    return { success: true, reward: 200 };
-}
