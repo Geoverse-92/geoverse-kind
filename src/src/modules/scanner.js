dd76672330d90762cfe569a89e8fd634eb2790af
Geoverse-92 <m.gajdzicki92@gmail.com>
2026-09-07 11:28:44 +0200

    Change receipt icon in processReceipt function
    Signed-off-by: Geoverse-92 <m.gajdzicki92@gmail.com>

--- src/src/modules/scanner.js
+++ src/src/modules/scanner.js
@@ -2,8 +2,8 @@ import { AppState } from '../core/state.js';
 
 export function processReceipt(receiptCode) {
     const state = AppState.load();
+    state.inventory.push({ name: receiptCode, icon: "🧾" });
     state.player.gold += 200;
-    state.inventory.push({ name: receiptCode, icon: "🎫" });
     AppState.save(state);
     return { success: true, reward: 200 };
 }
